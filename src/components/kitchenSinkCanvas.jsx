199d59c82dbf67a6b05e1b6fb602410ae30d3236
Talha <talhaiftikhar12549@gmail.com>
2024-07-30 17:37:24 +0500

    Added Image in Fabric Js in Version 6

--- src/components/kitchenSinkCanvas.jsx
+++ src/components/kitchenSinkCanvas.jsx
@@ -1,5 +1,6 @@
 import * as fabric from 'fabric';
 import {useEffect} from "react"
+import img1 from "../assets/download.png"
 
 let canvas;
 
@@ -11,8 +12,8 @@ export default function Meow() {
                 backgroundColor: "#f1f1f1",
                 height: 400,
                 selectionBorderColor: "black"
-
             });
+            window.canvas = canvas
             canvas.renderAll();
         }
     }, [])
@@ -30,13 +31,82 @@ export default function Meow() {
         canvas.renderAll();
     }
 
+    function addTriangle() {
+        let circle = new fabric.Circle({
+            radius: 20,
+            fill: 'green',
+            left: 100,
+            top: 100
+        });
+        canvas.add(circle)
+        canvas.renderAll()
+    }
+
+    function addImage() {
+        let sImg = img1
+        let imgInstance = new fabric.Image(sImg, {
+            left: 100,
+            top: 100,
+            angle: 30,
+            opacity: 0.85
+        });
+        canvas.add(imgInstance);
+        canvas.renderAll()
+    }
+
+
+    // const AddImage = () => {
+    //     var crossImg = new Image();
+    //     crossImg.onload = function (img) {
+    //         var cross = new fabric.FabricImage(crossImg, {
+    //             left: 300,
+    //             top: 150,
+    //             scaleX: 1,
+    //             scaleY: 1
+    //         });
+    //         fabricCanvasRef.current.add(cross);
+    //     };
+    //     crossImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDrKL3RBmogp-LQGw48bJ6KHjMgjWwmLFpcQ&s";
+    // };
+
+    function addImageUrl() {
+        console.log("meow")
+        // let crossImg = new Image();
+        // crossImg.onload = function (img) {
+        //     let cross = new fabric.FabricImage(crossImg, {
+        //         left: 300,
+        //         top: 150,
+        //         scaleX: 1,
+        //         scaleY: 1
+        //     })
+        //     fabricCanvasRef.current.add(cross);
+        //     crossImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDrKL3RBmogp-LQGw48bJ6KHjMgjWwmLFpcQ&s";
+        // }
+
+        // const newImage = new Image()
+        // newImage.src = img1;
+        // newImage.crossOrigin = 'Anonymous';
+        // debugger
+        // newImage.onload = () => {
+        //     fabric.Image.fromURL(newImage.src)
+        // }
+
+
+        fabric.Image.fromURL('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDrKL3RBmogp-LQGw48bJ6KHjMgjWwmLFpcQ&s', {crossOrigin: 'anonymous'}, {}).then(ob => {
+            canvas.add(ob)
+            canvas.renderAll();
+        })
+    }
 
     return (
         <>
             <div className={"flex w-[100%] h-[70vh] justify-between"}>
                 <div className={"w-[20%]"}>
                     meow
-                    <button onClick={addRectangle} className={"border"}>press me</button>
+                    <button onClick={addRectangle} className={"border"}>Add Rectangle</button>
+                    <button onClick={addTriangle} className={"border"}>Add Triangle</button>
+                    <button onClick={addImage} className={"border"}>Add Image</button>
+                    <button onClick={addImageUrl} className={"border"}>Add Image from Url</button>
                 </div>
                 <div className={" "}>
                     <canvas id="canvas">
